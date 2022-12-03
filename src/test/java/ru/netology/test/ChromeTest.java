0fa7561d670626aedfc03a6fcbb600e276ea447c
Vito <vito_bjj@mail.ru>
2022-12-03 20:48:59 +0300

    Add code in ChromeTest

--- src/test/java/ru/netology/test/ChromeTest.java
+++ src/test/java/ru/netology/test/ChromeTest.java
@@ -4,10 +4,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.shadow.com.univocity.parsers.conversions.Conversions.trim;
+
 
 class ChromeTest {
 
@@ -15,7 +18,7 @@ class ChromeTest {
 
     @BeforeAll
     static void setupAll() {
-        WebDriverManager.chromedriver().setup();
+        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
     }
 
     @BeforeEach
@@ -26,11 +29,21 @@ class ChromeTest {
     @AfterEach
     void teardown() {
         driver.quit();
+        driver = null;
     }
 
     @Test
     void test() {
-        // Your test logic here
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("span[data-test-id=name] input")).sendKeys("Карен Карамян");
+        driver.findElement(By.cssSelector("span[data-test-id=phone] input")).sendKeys("+78969865123");
+        driver.findElement(By.cssSelector("label[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("p[data-test-id=order-success]")).getText().trim();
+        assertEquals(expected, actual);
+
+
     }
 
 }
