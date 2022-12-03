50888d6f6c612b35ea256457b5d1da5922110525
Vito <vito_bjj@mail.ru>
2022-12-03 22:13:03 +0300

    Fix code for headless

--- src/test/java/ru/netology/test/ChromeTest.java
+++ src/test/java/ru/netology/test/ChromeTest.java
@@ -26,6 +26,12 @@ class ChromeTest {
     @BeforeEach
     void setup() {
         driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
