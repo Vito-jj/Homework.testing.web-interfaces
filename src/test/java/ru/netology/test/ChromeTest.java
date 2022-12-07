df2ea7b6f4f4574ab4eedba19fb66095df9b3c25
Vito-jj <111686304+Vito-jj@users.noreply.github.com>
2022-12-07 20:17:15 +0300

    Update ChromeTest.java

--- src/test/java/ru/netology/test/ChromeTest.java
+++ src/test/java/ru/netology/test/ChromeTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,7 +21,7 @@ class ChromeTest {
 
     @BeforeAll
     static void setupAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
